90091ca570d45f95ce308b52c6d1a262d0bbdcdd
Chris Richardson <chris@bpi.cam.ac.uk>
2016-01-01 14:04:09 +0000

    Fix to read multi-directory tifs

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 # README #
 
 This is a simple code to convert a voxel mesh in tiff format to a tetrahedral mesh.
-First extract the tiff to multiple files (maybe not needed but I couldn't figure out how else to do it quickly).
 
-```
-convert muntjac_femur.tif muntjac_femur-%04d.tif
-```
 
-The main code is configured with cmake. Needs libtiff, CGAL and boost.
+The main code is configured with cmake. Needs libtiff and CGAL.
 
 
 The output is in `.mesh` format which is a bit obscure. To convert to dolfin format (using dolfin-convert), you will
-have to edit it a bit. The line "Dimension 3" has to be replaced with "Dimension" (newline) "3" (newline). And all triangles will need to be deleted. This is annoying, and should be fixed in 
-dolfin-convert
+have to edit it a bit. The line "Dimension 3" has to be replaced with "Dimension" (newline) "3" (newline). And all triangles will need to be deleted. This is annoying, and should be fixed in dolfin-convert
--- main.cpp
+++ main.cpp
@@ -30,74 +30,79 @@ using namespace CGAL::parameters;
 
 #include <CGAL/ImageIO.h>
 
-#include <sstream>
-#include <iomanip>
-#include <boost/filesystem.hpp>
+#include <cassert>
 #include <tiffio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  std::vector<unsigned char> image_data;
+  std::string filename = "muntjac_femur.tif";
+ 
+  TIFF* tif = TIFFOpen(filename.c_str(), "r");
+  int ndir = 1;
+  uint32 w = -1, h = -1;
 
-  std::size_t i = 0;
-  uint32 w, h;
-  bool file_exists = true;
-  while(file_exists)
-    {
-      std::stringstream s;
-      s << "muntjac_femur-";
-      s << std::setfill('0') << std::setw(4) << i;
-      s << ".tif";
-
-      std::cout << s.str() << "\n";
-
-      file_exists = boost::filesystem::exists(s.str());
-      if (file_exists)
+  if (tif)
+    {      
+      bool tif_ok = true;
+      while (tif_ok)
+      {
+	if (tif_ok = TIFFReadDirectory(tif))
 	{
-          TIFF* tif = TIFFOpen(s.str().c_str(), "r");
-          if (tif) 
-	    {
-	      std::size_t npixels;
-	      uint32* raster;
-	      
-	      TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
-	      TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
-	      std::cout << w << "*" << h << "\n";
-	      npixels = w * h;
-	      raster = (uint32*) _TIFFmalloc(npixels * sizeof (uint32));
-	      if (raster != NULL)
-		{
-		  if (TIFFReadRGBAImage(tif, w, h, raster, 0))
-		    {
-		      image_data.insert(image_data.end(), raster, raster + npixels);
-		    }
-		  _TIFFfree(raster);
-		}
-	      TIFFClose(tif);
-	    }
+	  ++ndir;
+	  uint32 wtmp, htmp;
+	  TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &wtmp);
+	  TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &htmp);
+	  
+	  if (w==-1 and h==-1)
+	  { 
+	    w = wtmp; 
+	    h = htmp;
+	  }
+	  else
+	  {
+	    assert(w == wtmp);
+	    assert(h == htmp);
+	  }
 	}
-      ++i;	
+      }
+      TIFFClose(tif);
     }
-  // Got 3D image
-  
-  int nx=w, ny=h, nz=i;
-  double vx=1.0, vy=1.0, vz=1.0;
+
+  int nx = w, ny = h, nz = ndir;
+  double vx = 1.0, vy = 1.0, vz = 1.0;
 
   std::cout << nx << "x" << ny << "x" << nz << std::endl;
-  
+
   CGAL::Image_3 image(_createImage(nx, ny, nz, 1,
 				   vx, vy, vz, 1,
 				   WK_FIXED, SGN_UNSIGNED));
 
-  // Invert image
-  for (auto &p : image_data)
-    p = 255 - p;
-
   unsigned char *ptr = (unsigned char *)(image.data());
-  std::copy(image_data.begin(), image_data.end(), ptr);
+  const std::size_t npixels = w*h;
+  tif = TIFFOpen(filename.c_str(), "r");
+  assert(tif);
+  for (int i = 0 ; i < ndir; ++i)
+  {
+    uint32* raster = (uint32*) _TIFFmalloc(npixels * sizeof (uint32));
+    if (raster != NULL)
+    {
+      if (TIFFReadRGBAImage(tif, w, h, raster, 0))
+      {
+	std::copy(raster, raster + npixels, ptr);
+	ptr += npixels;
+      }
+      _TIFFfree(raster);
+    }
+    TIFFReadDirectory(tif);    
+  }
 
-  std::cout << "max=" << (int)(*std::max_element(ptr, ptr+nx*ny*nz)) << "\n";
-  std::cout << "min=" << (int)(*std::min_element(ptr, ptr+nx*ny*nz)) << "\n";
+  // Invert image
+  unsigned char *p = (unsigned char *)(image.data());
+  for (std::size_t i = 0; i != (nx*ny*nz); ++i)
+  {
+    *p = 255 - *p;
+    ++p;
+  }
 
   // Domain
   Mesh_domain domain(image);
